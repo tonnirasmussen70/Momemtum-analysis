6942a8fef118740f32d12668e81994c6071e2539
tonnirasmussen70 <tonni.rasmussen70@gmail.com>
2026-05-31 19:40:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if portfolio.empty:
 
 st.subheader("Porteføljeinput")
 
-input_cols = ["ETF_Navn", "Exposure", "Quantity", "InputPrice", "Weight", "Sector"]
+input_cols = ["ETF_Navn", "Quantity", "Exposure", "InputPrice", "Weight", "Sector"]
 input_cols = [c for c in input_cols if c in portfolio.columns]
 portfolio_display = portfolio[input_cols].copy()
 
